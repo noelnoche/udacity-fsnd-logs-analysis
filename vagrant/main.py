04b3b4164a959195e025ea49aaba28ac0d0657da
Noel Noche <noelnoche@users.noreply.github.com>
2017-09-13 02:04:26 -0700

    Delete dead code

--- vagrant/main.py
+++ vagrant/main.py
@@ -22,26 +22,9 @@ TUP2 = ("Here are the most popular authors (total views): ",
 TUP3 = ("Here are the dates when 404 errors exceeded 1%: ",
         "select date, percent_error from error_reporter;")
 
-# queries_array = [qry1, qry2, q3v, qry3]
+# Queries for creating views
 VIEWS_SQL = open("views.sql", "r").read()
 QUERIES_ARRAY = [TUP1, TUP2, TUP3]
-
-# class ReportLog(object):
-#     """Database interface.
-#     This class provides the interface for making and filtering SQL
-#     queries and displaying the formatted results.
-
-#     Args:
-#         queryarray (:obj:`list` of :obj: `str`): Array of SQL queries.
-#         viewssarray (:obj:`list` of :obj: `tuple`): Array of tuples,
-#         with each tuple containing a string for the question and a
-#         string to query the view tables.
-#     """
-
-#     def __init__(self, viewssql, queriesarray):
-#         self.views_sql = viewssql
-#         self.queries_array = queriesarray
-#         self.data_ready = False
 
 
 def print_table(ques, data):
@@ -77,10 +60,6 @@ def process_db(init):
         cur = db_conn.cursor()
 
         if init:
-
-            # Create the views.
-            # for qry in self.queries_array:
-            #     cur.execute(qry)
             cur.execute(VIEWS_SQL)
         else:
 
@@ -108,6 +87,7 @@ def process_db(init):
             db_conn.close()
             sys.exit(0)
 
+
 def initialize():
     """"Starts the database query process."""
 
@@ -119,63 +99,5 @@ def initialize():
             break
 
 
-def run_main():
-    """Executes the program using the global variables.
-    The variables contain the SQL queries that are to be passed nto the
-    ReportLog interface.
-    """
-
-    # Adds a view for the top 3 articles in the database.
-    # qry1 = ("create or replace view top_articles as "
-    #         "select articles.title, count(log.path) as views from articles, "
-    #         "log where log.status = '200 OK' and log.path != '/' and "
-    #         "log.path like concat('%', articles.slug) group by "
-    #         "articles.title order by views desc limit 3;")
-
-    # Adds a view for author viewer ranking.
-    # qry2 = ("create or replace view top_authors as "
-    #         "select authors.name, subsq.views from authors, "
-    #         "(select articles.author, count(log.path) as views from articles, "
-    #         "log where log.status = '200 OK' and log.path != '/' and "
-    #         "log.path like concat('%', articles.slug) group by articles.author "
-    #         "order by views desc) as subsq where authors.id = subsq.author;")
-
-    # Creates views that are used in qry3.
-    # q3v = ("create or replace view total_view as "
-    #        "select to_char(time, 'YYYY-MM-DD') as date, count(log.status) "
-    #        "as total_requests from log group by date order by date desc;"
-    #        "create or replace view failed_view as "
-    #        "select to_char(time, 'YYYY-MM-DD') as date, count(log.status) "
-    #        "as failed_requests from log where log.status = '404 NOT FOUND' "
-    #        "group by date order by date desc;")
-
-    # Creates a view for dates when failed requests exceed 1 percent.
-    # qry3 = ("create or replace view error_reporter as "
-    #         "select f.date as date, "
-    #         "round((f.failed_requests/t.total_requests::float * 100)::numeric,"
-    #         "2) as percent_error from (select * from failed_view) as f, "
-    #         "(select * from total_view) as t where f.date = t.date and "
-    #         "f.failed_requests/t.total_requests::float * 100.00 > 1;")
-
-    # tup1, tup2, tup3: Tuple data for questions and answers.
-    # tup1 = ("Here are the three top articles (total views): ",
-    #         "select title, views from top_articles;")
-
-    # tup2 = ("Here are the most popular authors (total views): ",
-    #         "select name, views from top_authors;")
-
-    # tup3 = ("Here are the dates when 404 errors exceeded 1%: ",
-    #         "select date, percent_error from error_reporter;")
-
-    # queries_array = [qry1, qry2, q3v, qry3]
-    # views_sql = open("views.sql", "r").read()
-    # queries_array = [tup1, tup2, tup3]
-
-    # logsobj = ReportLog(views_sql, queries_array)
-    # logsobj.initialize()
-
-
-
 if __name__ == "__main__":
-    # run_main()
     initialize()
